301046759cc6ebc0c5daa1f43d9c7fd7c0f3900e
Parygind <parygind@gmail.com>
2015-09-12 19:30:36 +0600

    Работа с шейдерами

---
+++ app/src/main/java/com/shasser/froggy/Texture.java
@@ -0,0 +1,43 @@
+package com.shasser.froggy;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+
+public class Texture {
+
+    private int name;
+
+    public Texture(Context context, int idpicture) {
+
+        int []names = new int[1];
+        GLES20.glGenTextures(1, names, 0);
+        name = names[0];
+        GLES20.glPixelStorei(GLES20.GL_UNPACK_ALIGNMENT, 1);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, name);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MIN_FILTER,
+                GLES20.GL_NEAREST_MIPMAP_NEAREST);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MAG_FILTER,
+                GLES20.GL_NEAREST);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_S,
+                GLES20.GL_REPEAT);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_T,
+                GLES20.GL_REPEAT);
+        Bitmap bitmap =
+                BitmapFactory.decodeResource(context.getResources(), idpicture);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+        bitmap.recycle();
+        GLES20.glGenerateMipmap(GLES20.GL_TEXTURE_2D);
+    }
+
+    public int getName() {
+        return name;
+    }
+}
